f6f61c56ba5e36c251248e87e6d9cad347a0794d
Brett Lawson <brett19@gmail.com>
2014-10-02 01:36:32 -0300

    GCSE styling fixups.

--- assets/javascripts/init.js
+++ assets/javascripts/init.js
@@ -1,5 +1,14 @@
 (function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement);
 
+/* Added placeholder for GCSE */
+window.__gcse = {
+	callback: function() {
+		var gcse_el = document.getElementById('gsc-i-id1');
+		gcse_el.setAttribute('placeholder', 'Search Documentation');
+	}
+};
+
+/* Load Vendor Things */
 (function(d,t) {
 	if ('querySelector' in d && 'addEventListener' in window) {
 		r=d.createElement(t);s=d.querySelector(t);
